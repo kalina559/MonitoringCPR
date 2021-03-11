1a4ab908de6ccdab6d4dae2700ccf6df99786d15
Jakub Kalinowski <kalina559@gmail.com>
2021-03-11 20:26:42 +0100

    Added compression measurements
    - angles in elbows, and between arms and floor
    - compression count and frequency
    - markers connected by cylinders

---
+++ MonitoringCPRUnityProject/Assets/Scripts/Cylinder.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cylinder : MonoBehaviour
+{
+    public GameObject firstMarker, secondMarker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = (firstMarker.transform.position - secondMarker.transform.position) / 2.0f + secondMarker.transform.position;
+
+        var v3T = transform.localScale;      // Scale it
+        v3T.y = (firstMarker.transform.position - secondMarker.transform.position).magnitude/2;
+        transform.localScale = v3T;
+
+
+        // Rotate it
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, firstMarker.transform.position - secondMarker.transform.position);
+    }
+}
--- MonitoringCPRUnityProject/Assets/Scripts/GetMarkerCoordinates.cs
+++ MonitoringCPRUnityProject/Assets/Scripts/GetMarkerCoordinates.cs
@@ -8,36 +8,62 @@ using TMPro;
 public class GetMarkerCoordinates : MarkerTracking
 {
     public List<CvCoordinates> initialCoordinates;
-    CvCoordinates offset;
-    bool offsetSet = false;
-    //Vector3 startingPosition;
+    public List<GameObject> cylinders;
+    Plane floorPlane;
+    Plane armsPlane;
+    public GameObject plane;
+    public TextMeshProUGUI firstElbowAngle;
+    public TextMeshProUGUI secondElbowAngle;
+    public TextMeshProUGUI armAngle;
+    public TextMeshProUGUI compressionsRate;
+    public TextMeshProUGUI compressionsCount;
+
+    bool DownwardMovement = false;
+    int downwardMovementFrameCount = 0;
+    bool firstMeasurement = true;
+    int compressionCount = 0;
+    float lastYCoordinate = 0;
+    float trackingStartTime;
+
     private void Start()
     {
-        expectedNumberOfMarkerPairs = 5;
-        //startingPosition = new Vector3(0.23f, 0.8f, -9.28f);
+        expectedNumberOfMarkerPairs = 8;
         initializeScene();
+        floorPlane = new Plane();
     }
     protected override void useMarkerCoordinates()
     {
-        if(offsetSet == false)
-        {
-            offset = new CvCoordinates();
-            saveOffset();
-            offsetSet = true;
-        }
         for (int i = 0; i < _balls.Length; i++)
-        {  
-            markers[i].transform.position = 
-                new Vector3(_balls[i].X - offset.X,
-                -_balls[i].Y - offset.Y,
-                _balls[i].Z - offset.Z);
+        {
+            markers[i].transform.position =
+                new Vector3(_balls[i].X,
+                -_balls[i].Y,
+               _balls[i].Z);
         }
-    }
-    public void saveOffset()
-    {
-        offset.X = _balls[4].X - 1f;
-        offset.Y = -_balls[4].Y + 0.2f;
-        offset.Z = _balls[4].Z + 10.3f;
+
+
+
+
+        floorPlane.Set3Points(
+            markers[7].transform.position,
+            markers[6].transform.position,
+            markers[5].transform.position);
+
+        plane.transform.position = new Vector3(markers[6].transform.position.x, markers[6].transform.position.y, markers[6].transform.position.z);
+        plane.transform.rotation = Quaternion.FromToRotation(Vector3.up, floorPlane.normal);
+        Camera.main.transform.LookAt(markers[7].transform.position);
+
+        calculateAngles();
+
+        if (firstMeasurement == true)
+        {
+            firstMeasurement = false;
+        }
+        else
+        {
+            checkCompressionParameters();
+            lastYCoordinate = markers[4].transform.position.y;
+        }
     }
     public override void changeMode()
     {
@@ -45,12 +71,63 @@ public class GetMarkerCoordinates : MarkerTracking
         {
             beginTracking = true;
             frameCount = 0;
+            compressionCount = 0;
+            trackingStartTime = Time.time; 
         }
         else
         {
             beginTracking = false;
-            offsetSet = false;
         }
+    }
+
+    void calculateAngles()
+    {
+        var rightElbowFirstVec = markers[0].transform.position - markers[2].transform.position;
+        var rightElbowSecondVec = markers[2].transform.position - markers[4].transform.position;
+        firstElbowAngle.SetText("K¹t w prawym ³okciu: " + Vector3.Angle(rightElbowFirstVec, rightElbowSecondVec));
+
+        var leftElbowFirstVec = markers[1].transform.position - markers[3].transform.position;
+        var leftElbowSecondVec = markers[3].transform.position - markers[4].transform.position;
+        secondElbowAngle.SetText("K¹t w lewym ³okciu: " + Vector3.Angle(leftElbowFirstVec, leftElbowSecondVec));
+
+        armsPlane.Set3Points(
+           markers[0].transform.position,
+           markers[1].transform.position,
+           markers[4].transform.position);
+
+        armAngle.SetText("K¹t miêdzy rêkami, a pod³og¹: " + Vector3.Angle(floorPlane.normal, armsPlane.normal));
+    }
+
+    void checkCompressionParameters()
+    {
+        if (Math.Abs(markers[4].transform.position.y - lastYCoordinate) > 0.001)
+        {
+            if (markers[4].transform.position.y < lastYCoordinate)
+            {
+                if (DownwardMovement == false)
+                {
+                    ++downwardMovementFrameCount;
+                    if (downwardMovementFrameCount == 10)
+                    {
+                        DownwardMovement = true;
+                    }
+                }
+            }
+            else if (markers[4].transform.position.y > lastYCoordinate)
+            {
+                downwardMovementFrameCount = 0;
+
+                if (DownwardMovement == true)
+                {
+                    downwardMovementFrameCount = 0;
+                    DownwardMovement = false;
+                    ++compressionCount;
+                    Debug.Log("liczba uciœniêæ: " + compressionCount);
+                    compressionsCount.SetText("Liczba uciœniêæ: " + compressionCount);
+                    compressionsRate.SetText("Czêstotliwoœæ: " + compressionCount * 60 / (Time.time - trackingStartTime) + " uciœniêæ/minutê");
+                }
+            }
+        }        
     }
 }
 
