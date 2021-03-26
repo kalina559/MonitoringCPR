cb28671186bf309d29e364672da601af4873d02b
Jakub Kalinowski <kalina559@gmail.com>
2021-03-26 15:04:02 +0100

    Updated powershell script

--- get_dll_files.ps1
+++ get_dll_files.ps1
@@ -1,23 +1,23 @@
-[string]$sourceDirectory  = $env:OPENCV_DIR + "\bin\Debug\opencv_world411d.dll"
+[string]$sourceDirectory  = $env:OPENCV_DIR + "\bin\Debug\opencv_world451d.dll"
 [string]$destinationDirectory = $PSScriptRoot + "\MonitoringCPR\MonitoringCPR"
 Copy-item -Force -Recurse -Verbose $sourceDirectory -Destination $destinationDirectory
 
-[string]$sourceDirectory  = $env:OPENCV_DIR + "\bin\Release\opencv_world411.dll"
+[string]$sourceDirectory  = $env:OPENCV_DIR + "\bin\Release\opencv_world451.dll"
 [string]$destinationDirectory = $PSScriptRoot + "\MonitoringCPR\MonitoringCPR"
 Copy-item -Force -Recurse -Verbose $sourceDirectory -Destination $destinationDirectory
 
-[string]$sourceDirectory  = $env:OPENCV_DIR + "\bin\Debug\opencv_world411d.dll"
+[string]$sourceDirectory  = $env:OPENCV_DIR + "\bin\Debug\opencv_world451d.dll"
 [string]$destinationDirectory = $PSScriptRoot + "\MonitoringCPR\MonitoringCPR"
 Copy-item -Force -Recurse -Verbose $sourceDirectory -Destination $destinationDirectory
 
-[string]$sourceDirectory  = $env:OPENCV_DIR + "\bin\Release\opencv_world411.dll"
+[string]$sourceDirectory  = $env:OPENCV_DIR + "\bin\Release\opencv_world451.dll"
 [string]$destinationDirectory = $PSScriptRoot + "\MonitoringCPR\MonitoringCPR"
 Copy-item -Force -Recurse -Verbose $sourceDirectory -Destination $destinationDirectory
 
-[string]$sourceDirectory  = $env:OPENCV_DIR + "\bin\Debug\opencv_world411d.dll"
+[string]$sourceDirectory  = $env:OPENCV_DIR + "\bin\Debug\opencv_world451d.dll"
 [string]$destinationDirectory = $PSScriptRoot + "\MonitoringCPRUnityProject\Assets\Libraries"
 Copy-item -Force -Recurse -Verbose $sourceDirectory -Destination $destinationDirectory
 
-[string]$sourceDirectory  = $env:OPENCV_DIR + "\bin\Release\opencv_world411.dll"
+[string]$sourceDirectory  = $env:OPENCV_DIR + "\bin\Release\opencv_world451.dll"
 [string]$destinationDirectory = $PSScriptRoot + "\MonitoringCPRUnityProject\Assets\Libraries"
 Copy-item -Force -Recurse -Verbose $sourceDirectory -Destination $destinationDirectory
